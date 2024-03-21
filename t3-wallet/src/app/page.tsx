ed4c3dfd7ea1a914936a85e5bac1122e2496710e
Yeonsang Hwang <57931120+kiteprize@users.noreply.github.com>
2024-03-21 18:28:56 +0900

    feat: 중간 커밋

--- t3-wallet/src/app/page.tsx
+++ t3-wallet/src/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
@@ -75,8 +74,6 @@ async function CrudShowcase() {
       ) : (
         <p>You have no posts yet.</p>
       )}
-
-      <CreatePost />
     </div>
   );
 }
